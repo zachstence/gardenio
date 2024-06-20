486fbea61fa0c9cb1ad6ababba8eb3cd540602ef
Zachary Stence <zachstence@gmail.com>
2024-06-19 19:19:21 -0500

    feat: pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [
+		sveltekit(),
+		SvelteKitPWA({
+			manifest: { theme_color: '#426B29', background_color: '#ABDB8F' },
+			pwaAssets: { image: 'static/logo.svg' }
+		})
+	]
 });
